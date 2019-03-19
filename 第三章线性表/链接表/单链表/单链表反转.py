1d64bc5f6ef904d18c646a3d827057d2e493bee8
xuqil <13829842679@163.com>
2019-03-19 22:05:28 +0800

    单链表反转

--- 第三章线性表/链接表/单链表/单链表反转.py
+++ 第三章线性表/链接表/单链表/单链表反转.py
@@ -7,7 +7,7 @@ class Node:
 def rev(link):
     pre = link
     cur = link.next
-    pre.next = None
+    pre.next = None  # 第一个元素变为最后一个元素，它的next指向None
     while cur:
         temp = cur.next
         cur.next = pre
@@ -22,3 +22,20 @@ if __name__ == '__main__':
     while root:
         print(root.data)
         root = root.next
+
+
+"""
+line 9-11是将原链表的第一个节点变成了新链表的最后一个节点，同时将原链表的第二个节点保存在cur中
+
+line13-16就是从原链表的第二个节点开始遍历到最后一个节点，将所有节点翻转一遍
+
+以翻转第二个节点为例
+
+temp = cur.next是将cur的下一个节点保存在temp中，也就是第节点3，因为翻转后，节点2的下一个节点变成了节点1，原先节点2和节点3之间的连接断开，通过节点2就找不到节点3了，因此需要保存
+
+cur.next = pre就是将节点2的下一个节点指向了节点1
+
+然后pre向后移动到原先cur的位置，cur也向后移动一个节点，也就是pre = cur ,cur =temp
+
+这就为翻转节点3做好了准备
+"""
